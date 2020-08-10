376bc0e23bccff09e4f76afa4a25cc8c3724e26a
Abhishek Salgaonkar <asalgaonkar@rentpath.com>
2020-08-10 18:25:37 -0400

    Initial Commit

---
+++ DatingApp.API/Data/DataContext.cs
@@ -0,0 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using DatingApp.API.Model;
+namespace DatingApp.API.Data
+{
+    public class DataContext:DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options):base(options)
+        {
+            
+        }
+
+        public DbSet<Value> Values {get;set;}
+    }
+}
